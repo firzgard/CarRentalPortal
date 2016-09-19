2261e41ced597c24d10edc9693df9fe43083f366
megafirzen <megafirzen@gmail.com>
2016-09-19 21:13:05 +0700

    add new docs and complete searchResult mockup

--- web layout mockup/common/searchResult/searchResult.js
+++ web layout mockup/common/searchResult/searchResult.js
@@ -1,4 +1,193 @@
+const searchResultTemplate = $('#searchResultTemplate').html();
+Mustache.parse(searchResultTemplate); // Parse template before-hand. speeds up future uses
+
 $(document).ready(function(){
+	// This mockup data is the model for future ajax-loaded searchResult json data.
+	let mockupSearchResult = { searchResults: [
+		{
+			id: 1,
+			name: 'Koenigsegg Agera R',
+			licenseNumber: '29LD-6666',
+			perDayPrice: '666,666',
+			garageLocation: 'Ho Chi Minh',
+			garageName: 'AO Hoàng Hoa Thám',
+			imageURL: './../../static/img/vehicle/1/1.jpg',
+			vehicleURL: '#',
+			star: 1
+		},
+		{
+			id: 2,
+			name: 'Koenigsegg Agera R',
+			licenseNumber: '29LD-9999',
+			perDayPrice: '999,999',
+			garageLocation: 'Ho Chi Minh',
+			garageName: 'AO Hoàng Hoa Thám',
+			imageURL: './../../static/img/vehicle/1/1.jpg',
+			vehicleURL: '#',
+			star: 2
+		},
+		{
+			id: 3,
+			name: 'Koenigsegg Agera R',
+			licenseNumber: '29LD-6666',
+			perDayPrice: '666,666',
+			garageLocation: 'Ho Chi Minh',
+			garageName: 'AO Hoàng Hoa Thám',
+			imageURL: './../../static/img/vehicle/1/1.jpg',
+			vehicleURL: '#',
+			star: 3
+		},
+		{
+			id: 4,
+			name: 'Koenigsegg Agera R',
+			licenseNumber: '29LD-9999',
+			perDayPrice: '999,999',
+			garageLocation: 'Ho Chi Minh',
+			garageName: 'AO Hoàng Hoa Thám',
+			imageURL: './../../static/img/vehicle/1/1.jpg',
+			vehicleURL: '#',
+			star: 4
+		},
+		{
+			id: 5,
+			name: 'Koenigsegg Agera R',
+			licenseNumber: '29LD-6666',
+			perDayPrice: '666,666',
+			garageLocation: 'Ho Chi Minh',
+			garageName: 'AO Hoàng Hoa Thám',
+			imageURL: './../../static/img/vehicle/1/1.jpg',
+			vehicleURL: '#',
+			star: 5
+		},
+		{
+			id: 6,
+			name: 'Koenigsegg Agera R',
+			licenseNumber: '29LD-9999',
+			perDayPrice: '999,999',
+			garageLocation: 'Ho Chi Minh',
+			garageName: 'AO Hoàng Hoa Thám',
+			imageURL: './../../static/img/vehicle/1/1.jpg',
+			vehicleURL: '#',
+			star: 1
+		},
+		{
+			id: 7,
+			name: 'Koenigsegg Agera R',
+			licenseNumber: '29LD-6666',
+			perDayPrice: '666,666',
+			garageLocation: 'Ho Chi Minh',
+			garageName: 'AO Hoàng Hoa Thám',
+			imageURL: './../../static/img/vehicle/1/1.jpg',
+			vehicleURL: '#',
+			star: 2
+		},
+		{
+			id: 8,
+			name: 'Koenigsegg Agera R',
+			licenseNumber: '29LD-9999',
+			perDayPrice: '999,999',
+			garageLocation: 'Ho Chi Minh',
+			garageName: 'AO Hoàng Hoa Thám',
+			imageURL: './../../static/img/vehicle/1/1.jpg',
+			vehicleURL: '#',
+			star: 3
+		},
+		{
+			id: 9,
+			name: 'Koenigsegg Agera R',
+			licenseNumber: '29LD-6666',
+			perDayPrice: '666,666',
+			garageLocation: 'Ho Chi Minh',
+			garageName: 'AO Hoàng Hoa Thám',
+			imageURL: './../../static/img/vehicle/1/1.jpg',
+			vehicleURL: '#',
+			star: 4
+		},
+		{
+			id: 10,
+			name: 'Koenigsegg Agera R',
+			licenseNumber: '29LD-9999',
+			perDayPrice: '999,999',
+			garageLocation: 'Ho Chi Minh',
+			garageName: 'AO Hoàng Hoa Thám',
+			imageURL: './../../static/img/vehicle/1/1.jpg',
+			vehicleURL: '#',
+			star: 5
+		},
+		{
+			id: 11,
+			name: 'Koenigsegg Agera R',
+			licenseNumber: '29LD-6666',
+			perDayPrice: '666,666',
+			garageLocation: 'Ho Chi Minh',
+			garageName: 'AO Hoàng Hoa Thám',
+			imageURL: './../../static/img/vehicle/1/1.jpg',
+			vehicleURL: '#',
+			star: 1
+		},
+		{
+			id: 12,
+			name: 'Koenigsegg Agera R',
+			licenseNumber: '29LD-9999',
+			perDayPrice: '999,999',
+			garageLocation: 'Ho Chi Minh',
+			garageName: 'AO Hoàng Hoa Thám',
+			imageURL: './../../static/img/vehicle/1/1.jpg',
+			vehicleURL: '#',
+			star: 2
+		},
+		{
+			id: 13,
+			name: 'Koenigsegg Agera R',
+			licenseNumber: '29LD-6666',
+			perDayPrice: '666,666',
+			garageLocation: 'Ho Chi Minh',
+			garageName: 'AO Hoàng Hoa Thám',
+			imageURL: './../../static/img/vehicle/1/1.jpg',
+			vehicleURL: '#',
+			star: 3
+		},
+		{
+			id: 14,
+			name: 'Koenigsegg Agera R',
+			licenseNumber: '29LD-9999',
+			perDayPrice: '999,999',
+			garageLocation: 'Ho Chi Minh',
+			garageName: 'AO Hoàng Hoa Thám',
+			imageURL: './../../static/img/vehicle/1/1.jpg',
+			vehicleURL: '#',
+			star: 4
+		},
+		{
+			id: 15,
+			name: 'Koenigsegg Agera R',
+			licenseNumber: '29LD-6666',
+			perDayPrice: '666,666',
+			garageLocation: 'Ho Chi Minh',
+			garageName: 'AO Hoàng Hoa Thám',
+			imageURL: './../../static/img/vehicle/1/1.jpg',
+			vehicleURL: '#',
+			star: 5
+		},
+	]};
+
+	// Render search results
+	$('#searchResultList').html(
+		Mustache.render(searchResultTemplate, mockupSearchResult)
+	);
+
+	// Render star-rating
+	for (searchResult of mockupSearchResult.searchResults) {
+		$(`.search-result[data-vehicle-id="${searchResult.id}"] .star-rating`).barrating({
+			theme: 'fontawesome-stars',
+			initialRating: searchResult.star,
+			readonly: true
+		});
+	}
+
+	// =========================
+	// Render filters
+
 	// Brand-tree selector
 	$('#brandTree').jstree({
 		core: {
