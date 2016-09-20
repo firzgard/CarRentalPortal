886cb1c92c48943a678268ea2a17bec5e8776ea7
Nguyễn Tường Tâm <tamntse61384@fpt.edu.vn>
2016-09-20 14:53:47 +0700

    update Home.html

---
+++ web layout mockup/common/home/home.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+	// Chosen
+	$('#locationSelector').chosen({
+		no_results_text: "No result!"
+	});
+	$(".my_select_box").chosen({
+	    no_results_text: "Oops, nothing found!",
+	    width: "100px"
+	});
+	
+});
