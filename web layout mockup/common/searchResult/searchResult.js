625d4d608038df8e882696b1a561e83450435432
megafirzen <megafirzen@gmail.com>
2016-09-27 14:55:15 +0700

    Move renderStarRating and datatables' custom filters creator func to seperated files

--- web layout mockup/common/searchResult/searchResult.js
+++ web layout mockup/common/searchResult/searchResult.js
@@ -171,30 +171,10 @@ const mockupSearchResult = {
 	totalPages: 20
 };
 
-// html star icons
-const	fullStar = '<i class="fa fa-star"></i>',
-		halfStar = '<i class="fa fa-star-half-o"></i>',
-		emptyStar = '<i class="fa fa-star-o"></i>';
-
 // Renderers
 //==================================
 
 function renderSearchResultGrid(searchResults){
-	function renderStarRating(starRating){
-		for(var html = '', star = starRating, i = 0; i < 5; i++) {
-			if(star >= 1) {
-				html += fullStar;
-				star--;
-			} else if (star > 0) {
-				html += halfStar;
-				star--;
-			} else {
-				html += emptyStar;
-			}
-		}
-		return html += `&nbsp;<span class="badge">${starRating}</span>`
-	}
-
 	function renderSearchResult(searchResult){
 		return `<div class="col-lg-4" >
 			<a href="${searchResult.vehicleURL}">
