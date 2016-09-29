a296bfc21726aa7a8248eaddbdd6d8699b3d8111
thanhhc <thanhhcse@gmail.com>
2016-09-29 15:08:16 +0700

    update garageManagement

--- web layout mockup/provider/garageManagement/garageManagement.js
+++ web layout mockup/provider/garageManagement/garageManagement.js
@@ -1,24 +1,38 @@
 const mockupData = [
-	{ "id": 1, "name": "Garage1", "location": "Ho Chi Minh", "star": 4.7, "isActive": true},
-	{ "id": 2, "name": "Garage2", "location": "Ho Chi Minh", "star": 3.7, "isActive": false},
-	{ "id": 3, "name": "Garage3", "location": "Ho Chi Minh", "star": 1.7, "isActive": false},
-	{ "id": 4, "name": "Garage4", "location": "Ho Chi Minh", "star": 2, "isActive": false},
-	{ "id": 5, "name": "Garage5", "location": "Ho Chi Minh", "star": 3.3, "isActive": false},
-	{ "id": 6, "name": "Garage6", "location": "Ho Chi Minh", "star": 4.4, "isActive": false},
-	{ "id": 7, "name": "Garage7", "location": "Ha Noi", "star": 5, "isActive": true},
-	{ "id": 8, "name": "Garage8", "location": "Ha Noi", "star": 1, "isActive": true},
-	{ "id": 9, "name": "Garage9", "location": "Ha Noi", "star": 2, "isActive": true},
-	{ "id": 10, "name": "Garage10", "location": "Ha Noi", "star": 4.7, "isActive": true}
+	{ "id": 1, "name": "Garage1", "address": "13B Quang Trung, Go Vap District", "location": "Ho Chi Minh", "star": 4.7, "isActive": true},
+	{ "id": 2, "name": "Garage2", "address": "209 Nguyen Tat Thanh, District 7", "location": "Ho Chi Minh", "star": 3.7, "isActive": false},
+	{ "id": 3, "name": "Garage3", "address": "20 Thanh Thai, District 10", "location": "Ho Chi Minh", "star": 1.7, "isActive": false},
+	{ "id": 4, "name": "Garage4", "address": "121A Nguyen Kiem, Go Vap District", "location": "Ho Chi Minh", "star": 2, "isActive": false},
+	{ "id": 5, "name": "Garage5", "address": "22E Lu Gia, District 10", "location": "Ho Chi Minh", "star": 3.3, "isActive": false},
+	{ "id": 6, "name": "Garage6", "address": "222 Huynh Tan Phat, District 7", "location": "Ho Chi Minh", "star": 4.4, "isActive": false},
+	{ "id": 7, "name": "Garage7", "address": "23A Thanh Cong, Ba Dinh", "location": "Ha Noi", "star": 5, "isActive": true},
+	{ "id": 8, "name": "Garage8", "address": "69 Nguyen Chi Thanh, Cau Giay", "location": "Ha Noi", "star": 1, "isActive": true},
+	{ "id": 9, "name": "Garage9", "address": "456 Hoang Huy Giap, Cau Giay", "location": "Ha Noi", "star": 2, "isActive": true},
+	{ "id": 10, "name": "Garage10", "address": "98 Hang Ngang", "location": "Ha Noi", "star": 4.7, "isActive": true}
 ];
 
 $(document).ready(() => {
+    
+    // set toogling dropdown event for filter dropdown buttons
+	$('#multiFilter .filter-toggle').on('click', function (event) {
+		let dropdownContainer = $(this).parent();
+
+		if(dropdownContainer.hasClass('open')){
+			$('#multiFilter .filter-toggle').parent().removeClass('open');
+		} else {
+			$('#multiFilter .filter-toggle').parent().removeClass('open');
+			dropdownContainer.addClass('open');
+		}
+	});
+    
 	// Render table
-	$('#garages').DataTable({
+	let table = $('#garages').DataTable({
+        dom: "ltipr",
 		data: mockupData,
 		columnDefs: [
 			{
 				// Render stars
-				targets: 3,
+				targets: 4,
 				render: (data, type) => {
 					if(type === 'display'){
 						return renderStarRating(data);
@@ -28,7 +42,7 @@ $(document).ready(() => {
 			},
 			{
 				// Render status label
-				targets: 4,
+				targets: 5,
 				render: (data, type) => {
 					if(type === 'display'){
 						return `<div class="status-label" >
@@ -40,7 +54,7 @@ $(document).ready(() => {
 			},
 			{
 				// Render action button
-				targets: 5,
+				targets: 6,
 				render: (data, type, row) => {
 					return `<div class="btn-group" >
 						<button data-toggle="dropdown" class="btn btn-info dropdown-toggle" aria-expanded="false">
@@ -48,7 +62,7 @@ $(document).ready(() => {
 						</button>
 						<ul class="dropdown-menu">
 							<li><a href="./../garage/garage.html">Edit</a></li>
-							${row[4] ?
+							${row.isActive ?
 								`<li><a href="#" data-toggle="modal" data-target="#mdModal" data-action="deactivate" data-id="${row.id}" data-name="${row.name}" >Deactivate</a></li>`
 							:
 								`<li><a href="#" data-toggle="modal" data-target="#mdModal" data-action="activate" data-id="${row.id}" data-name="${row.name}" >Activate</a></li>`
@@ -61,7 +75,8 @@ $(document).ready(() => {
 		],
 		columns: [
 			{ name: 'ID', data: 'id', visible: false },
-			{ name: 'Name', title: 'Name', data: 'name', width: '55%' },
+			{ name: 'Name', title: 'Name', data: 'name', width: '25%' },
+			{ name: 'Address', title: 'Address', data: 'address', width: '30%' },
 			{ name: 'Location', title: 'Location', data: 'location', width: '15%' },
 			{ name: 'Stars', title: 'Stars', data: 'star', width: '10%' },
 			{ name: 'Status', title: 'Status', data: 'isActive', width: '10%' },
@@ -74,6 +89,18 @@ $(document).ready(() => {
 		]
 	});
 
+    // garage's name text filter
+    createTextFilter(table, $('#garageName'), 'Name');
+    // garage's address text filter
+    createTextFilter(table, $('#garageAdress'), 'Address');
+    // location checkbox filter
+    createCheckboxFilter(table, $('#location'), 3);
+    // rate range filter | datatype: float
+    createFloatRangeFilter(table, $('#rate'), 4);
+    // status checkbox filter
+    createCheckboxFilter(table, $('#status'), 5);
+    
+    
 	// Render confirmation modal for actions
 	$('#mdModal').on('show.bs.modal', function(event) {
 		let button = $(event.relatedTarget),
