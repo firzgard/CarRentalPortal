e3f162636ed25a58ff4692980c00de37f912d33c
megafirzen <megafirzen@gmail.com>
2016-10-02 22:17:46 +0700

    Init .net MVC5 solution

---
+++ CRP/CRP/Models/Entities/Repositories/GarageRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRP.Models.Entities.Repositories
+{
+	public partial interface IGarageRepository : IRepository<Vehicle>
+	{
+
+	}
+
+	public partial class GarageRepository : BaseRepository<Vehicle>, IGarageRepository
+	{
+		public GarageRepository(CRPEntities entities) : base(entities) { }
+	}
+}
---
+++ CRP/CRP/Models/Entities/Services/VehicleService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using CRP.Models.Entities.Repositories;
+
+namespace CRP.Models.Entities.Services
+{
+	public interface IVehicleService : IService<Vehicle>
+	{
+		IQueryable<Vehicle> GetActive();
+		IQueryable<VehicleDetails> GetDetailList();
+
+		Task<VehicleDetails> GetDetailAsync(int? id);
+
+		Task<VehicleDetails> GetDetailAsync(string seoName);
+
+		Task CreateAsync(Vehicle vehicle);
+
+		Task UpdateAsync(Vehicle vehicle);
+	}
+
+	public class VehicleService
+	{
+	}
+
+	public class VehicleDetails
+	{
+		public Vehicle Vehicle { get; set; }
+		public IQueryable<ModelDetails> ModelDetails { get; set; }
+		public IQueryable<Garage> Garage { get; set; }
+		public IQueryable<VehicleGroup> VehicleGroup { get; set; }
+		public IQueryable<VehicleImage> VehicleImages { get; set; }
+	}
+}
