af7ca15e8a78bf4761d8993cf73d2704d9887288
Nguyễn Tường Tâm <tamntse61384@fpt.edu.vn>
2016-10-03 16:31:57 +0700

    asas

--- CRP/CRP/Models/Entities/Services/GarageService.cs
+++ CRP/CRP/Models/Entities/Services/GarageService.cs
@@ -6,10 +6,7 @@ using System.Web;
 
 namespace CRP.Models.Entities.Services
 {
-	public interface IGarageService : IService<Garage>
-	{
-	}
-	public class GarageService : BaseService<Garage>, IGarageService
+	public class GarageService 
 	{
         GarageRepository _repository = new GarageRepository();
         public Boolean add(Garage brand)
@@ -42,6 +39,7 @@ namespace CRP.Models.Entities.Services
                 catch (Exception e)
                 {
                     e.GetHashCode();
+                    return false;
                 }
                 return true;
             }
