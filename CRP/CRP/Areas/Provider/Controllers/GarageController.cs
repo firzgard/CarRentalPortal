82793002afd66f92f040e4d4aa00361c789a47b3
Nguyễn Tường Tâm <tamntse61384@fpt.edu.vn>
2016-10-03 15:24:28 +0700

    Brand ang garage

--- CRP/CRP/Areas/Provider/Controllers/GarageController.cs
+++ CRP/CRP/Areas/Provider/Controllers/GarageController.cs
@@ -4,14 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using CRP.Models;
+using CRP.Models.Entities.Services;
+using CRP.Models.Entities;
 
 namespace CRP.Areas.Provider.Controllers
 {
     public class GarageController : Controller
     {
-        // GET: Provider/Garage
+        GarageService service = new GarageService();
+        // GET: CarBrand
         public ActionResult Index()
         {
+            List<Garage> lstGara = new List<Garage>();
+            lstGara = service.getAll();
+            ViewBag.garaList = lstGara;
             return View();
         }
     }
