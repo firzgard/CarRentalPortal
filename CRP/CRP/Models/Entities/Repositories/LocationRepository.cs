71fc1d0779115876eb5ab910d16a88cc62ee58a9
Nguyễn Tường Tâm <tamntse61384@fpt.edu.vn>
2016-10-05 14:48:35 +0700

    Garacontroler, them Local cac kieu

---
+++ CRP/CRP/Models/Entities/Repositories/LocationRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRP.Models.Entities.Repositories
+{
+    public class LocationRepository : IBaseRepository<Location>
+    {
+        CRPEntities _dataContext = new CRPEntities();
+        public IEnumerable<Location> List
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public List<Location> getAllLocation()
+        {
+            List<Location> lstLocation = _dataContext.Locations.ToList<Location>();
+            return lstLocation;
+        }
+        
+        public void Add(Location entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Location entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Location findById(int Id)
+        {
+            var query = (from r in _dataContext.Locations where r.ID == Id select r).FirstOrDefault();
+            return query;
+        }
+
+        public void Update(Location entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CRP/CRP/Models/Entities/Services/LocationService.cs
@@ -0,0 +1,33 @@
+﻿using CRP.Models.Entities.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRP.Models.Entities.Services
+{
+    public class LocationService
+    {
+        LocationRepository _repository = new LocationRepository();
+        public Location findByID(int id)
+        {
+            Location location = _repository.findById(id);
+            return location;
+        }
+        public String findLocalNameByID(int id)
+        {
+            Location location = _repository.findById(id);
+            if (location == null)
+            {
+                return "SomeWhere";
+            }
+            return location.Name;
+        }
+        public List<Location> getAll()
+        {
+            List<Location> lstLocation = new List<Location>();
+            lstLocation = _repository.getAllLocation();
+            return lstLocation;
+        }
+    }
+}
