7baa04896dfe2e1bfa58b24da9c6e58121b8bef8
thanhhc <thanhhcse@gmail.com>
2016-10-05 13:48:13 +0700

    temp

---
+++ CRP/CRP/Models/ViewModels/AddVehicleGroupViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CRP.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRP.Models.ViewModels
+{
+    public class AddVehicleGroupViewModel : VehicleGroup
+    {
+        public AddVehicleGroupViewModel() : base() { }
+
+        //public AddVehicleGroupViewModel(VehicleGroup entity) : base(entity) { }
+
+        [Display(Name = "")]
+        public override string Name { get; set; }
+    }
+}
