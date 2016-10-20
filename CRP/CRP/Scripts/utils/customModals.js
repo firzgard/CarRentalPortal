9c4055ce430eedffa5f98821c60f03c69f9c8218
Nguyễn Tường Tâm <tamntse61384@fpt.edu.vn>
2016-10-20 15:53:46 +0700

    s

--- CRP/CRP/Scripts/utils/customModals.js
+++ CRP/CRP/Scripts/utils/customModals.js
@@ -1,4 +1,4 @@
-﻿const mockupGarages = [
+const mockupGarages = [
 	{id: 1, name: 'Garage Black'},
 	{id: 2, name: 'Garage White'},
 	{id: 3, name: 'Garage Red'},
@@ -279,7 +279,7 @@ function renderSelectorOptions(type, selectedID, html = ''){
         });
     }
 
-    function renderConfirmModal(type, action, modalNode, items){
+    function renderConfirmModal(table, type, action, modalNode, items){
         modalNode.innerHTML = `<div class="modal-dialog" role="document">
 		<div class="modal-content">
 			<div class="modal-header red-bg">
@@ -287,26 +287,33 @@ function renderSelectorOptions(type, selectedID, html = ''){
 					<span aria-hidden="true"><i class="fa fa-times-circle"></i></span>
 				</button>
 				<h2 class="modal-title">
-					${action === 'delete' ? 'Deletion'
-                            : (action === 'deactivate' ? 'Deactivation'
-                            : (action === 'activate' ? 'Activation'
-                            : ''))} Confirmation
+                Xác nhận
+					${action === 'delete' ? 'xóa'
+                            : (action === 'deactivate' ? 'hủy kích hoạt'
+                            : (action === 'reactivate' ? 'kích hoạt'
+                            : ''))}
 				</h2>
 			</div>
 			<div class="modal-body">
-				Có phải bạn muốn ${action} ${type}(s) này?:
+                Bạn có chắc chắn ${action === 'delete' ? 'xóa'
+                            : (action === 'deactivate' ? 'hủy kích hoạt'
+                            : (action === 'reactivate' ? 'kích hoạt lại'
+                            : ''))} ${type === 'vehicle group' ? 'nhóm giá'
+                                    : (type === 'garage' ? 'garage'
+                                    : (type === 'vehicle' ? 'xe'
+                                    : '')) } này?
 				<ul>${items.reduce((html, item) => {
 				    return html + `<li>${item.name}</li>`
 				}, '')}</ul>
 			</div>
 			<div class="modal-footer">
 				<button type="button" class="btn btn-default" data-dismiss="modal">Không</button>
-				<button type="button" class="btn btn-danger btn-yes">Đúng</button>
+				<button type="button" id="btn-yes" class="btn btn-danger btn-yes">Có</button>
 			</div>
 		</div>
 	</div>`;
 
-        $(document).on('click', '.btn-yes', function(event) {
+        $(document).one('click', '#btn-yes', function(event) {
             switch(type) {
                 case 'vehicle group':{
                     if (action === "deactivate" || action === "reactivate") {
@@ -316,10 +323,15 @@ function renderSelectorOptions(type, selectedID, html = ''){
                                 url: `/api/vehicleGroups/status/${id}`,
                                 type: "PATCH",
                                 success: function (data) {
-                                    alert("ok");
+                                    if(data.result) {
+                                        $('.modal').modal('hide');
+                                        table.ajax.reload();
+                                    } else {
+                                        alert("fail");
+                                    }
                                 },
                                 eror: function (data) {
-                                    alert("fail");
+                                    alert("error");
                                 }
                             });
                         }
@@ -331,10 +343,15 @@ function renderSelectorOptions(type, selectedID, html = ''){
                                 url: `/api/vehicleGroups/${id}`,
                                 type: "DELETE",
                                 success: function (data) {
-                                    alert("ok");
+                                    if(data.result) {
+                                        $('.modal').modal('hide');
+                                        table.ajax.reload();
+                                    } else {
+                                        alert("fail");
+                                    }
                                 },
                                 eror: function (data) {
-                                    alert("fail");
+                                    alert("error");
                                 }
                             });
                         }
@@ -348,12 +365,10 @@ function renderSelectorOptions(type, selectedID, html = ''){
                                 url: `/api/garage/status/${id}`,
                                 type: "PATCH",
                                 success: function (data) {
-                                    alert(data.message);
-                                    location.href = "/management/garageManagement"
+                                    alert("ok");
                                 },
                                 eror: function (data) {
                                     alert("fail");
-                                    location.href = "/management/garageManagement"
                                 }
                             });
                         }
@@ -365,11 +380,10 @@ function renderSelectorOptions(type, selectedID, html = ''){
                                 url: `/api/deleteGarage/${id}`,
                                 type: "DELETE",
                                 success: function (data) {
-                                    alert(data.message);
-                                    location.href = "/management/garageManagement";
+                                    alert("ok");
                                 },
                                 eror: function (data) {
-                                    alert("Còn Xe trong garage, vui lòng, di chuyển xe qua garage khác! Delete Failed!");
+                                    alert("fail");
                                 }
                             });
                         }
