8eefe77f09faa507b6dabe1bcf8a364074c4fa56
Nguyễn Tường Tâm <tamntse61384@fpt.edu.vn>
2016-10-24 12:37:08 +0700

    cacac

---
+++ CRP/CRP/Areas/Admin/Controllers/DashBoardController.cs
@@ -0,0 +1,40 @@
+﻿using CRP.Controllers;
+using CRP.Models.ViewModels;
+using CRP.Models.Entities;
+using CRP.Models.Entities.Repositories;
+using CRP.Models.Entities.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRP.Models;
+
+namespace CRP.Areas.Admin.Controllers
+{
+    public class DashBoardController : BaseController
+    {
+        // GET: Admin/DashBoard
+        public ActionResult Index()
+        {
+            ReportViewModel viewModel = new ReportViewModel();
+            var serviceBooking = this.Service<IBookingReceiptService>();
+            var serviceCar = this.Service<IVehicleService>();
+            var serviceUser = this.Service<IUserReceiptService>();
+            var serviceRole = this.Service<IRoleService>();
+            AspNetRole provider = serviceRole.Get("2");
+            viewModel.booking = serviceBooking.Get(q => q.IsPending == false).ToList().Count();
+            var bookSuccess = serviceBooking.Get(q => q.IsCanceled == false && q.IsPending == false).ToList().Count();
+            viewModel.bookingSuccess = bookSuccess;
+            viewModel.booking = serviceCar.Get().ToList().Count();
+            DateTime today = DateTime.Now;
+            //tinh co bookingfee co dinh hay 5%
+            viewModel.money = bookSuccess * 10000;
+            viewModel.provider = serviceUser.Get(q => q.AspNetRoles.Contains(provider)).ToList().Count();
+            viewModel.providerActive = serviceUser.Get(q => q.AspNetRoles.Contains(provider) && q.IsProviderUntil > today).ToList().Count();
+    
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ CRP/CRP/Models/ViewModels/ReportViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRP.Models.ViewModels
+{
+    public class ReportViewModel
+    {
+        public int booking { get; set; }
+        public int bookingSuccess { get; set; }
+        public int vehicle { get; set; }
+        public float money { get; set; }
+        public int provider { get; set; }
+        public int providerActive { get; set; }
+        public int customer { get; set; }
+        public int customerActive { get; set; }
+        public int garage { get; set; }
+        public int garageActive { get; set; }
+    }
+}
