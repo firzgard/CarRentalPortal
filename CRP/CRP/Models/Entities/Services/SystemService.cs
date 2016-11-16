3b10a617eb66c2b5bda8f05818e8b798096eaef9
thanhhc <thanhhcse@gmail.com>
2016-11-16 16:26:17 +0700

    save

--- CRP/CRP/Models/Entities/Services/SystemService.cs
+++ CRP/CRP/Models/Entities/Services/SystemService.cs
@@ -12,7 +12,7 @@ namespace CRP.Models.Entities.Services
 		//gui mail thong bao ve Booking moi
 		public void SendBookingAlertEmailToCustomer(BookingReceipt booking)
 		{
-			var mailMessage = new MailMessage("siunhandiendien11@gmail.com", booking.AspNetUser.Email)
+			var mailMessage = new MailMessage("crpservices1@gmail.com", booking.AspNetUser.Email)
 			{
 				Subject = "Giao dịch đặt xe mới trên hệ thống CRP",
 				Body = "Bạn vừa tiến hành đặt xe " + booking.VehicleName + " thành công trên hệ thống của chúng tôi.\nChúc bạn có một chuyến đi vui vẻ."
@@ -22,8 +22,8 @@ namespace CRP.Models.Entities.Services
 			{
 				Credentials = new System.Net.NetworkCredential()
 				{
-					UserName = "siunhandiendien11@gmail.com",
-					Password = "0975420837"
+					UserName = "crpservices1@gmail.com",
+					Password = "A123456z"
 				},
 				EnableSsl = true
 			};
@@ -33,7 +33,7 @@ namespace CRP.Models.Entities.Services
 		//gui mail thong bao ve Booking moi
 		public void SendBookingAlertEmailToProvider(BookingReceipt booking)
 		{
-			var mailMessage = new MailMessage("siunhandiendien11@gmail.com", booking.Garage.Email)
+			var mailMessage = new MailMessage("crpservices1@gmail.com", booking.Garage.Email)
 			{
 				Subject = "Giao dịch đặt xe mới trên hệ thống CRP",
 				Body = "Xe " + booking.VehicleName + " của bạn vừa được đặt từ " + booking.StartTime.ToString(@"dd\/MM\/yyyy HH:mm") + " đến " + booking.EndTime.ToString(@"dd\/MM\/yyyy HH:mm") + " bởi khách hàng " + booking.AspNetUser.FullName + ".\n"
@@ -44,8 +44,8 @@ namespace CRP.Models.Entities.Services
 			{
 				Credentials = new System.Net.NetworkCredential()
 				{
-					UserName = "siunhandiendien11@gmail.com",
-					Password = "0975420837"
+					UserName = "crpservices1@gmail.com",
+					Password = "A123456z"
 				},
 				EnableSsl = true
 			};
@@ -55,7 +55,7 @@ namespace CRP.Models.Entities.Services
 		//gui mail thong bao provider
 		public void SendBecomeProviderAlertEmail(string toEmail, AspNetUser user)
 		{
-			var mailMessage = new MailMessage("siunhandiendien11@gmail.com", toEmail)
+			var mailMessage = new MailMessage("crpservices1@gmail.com", toEmail)
 			{
 				Subject = "Giao dịch đăng ký quyền cung cấp trên hệ thống CRP",
 				Body = "Giao dịch đăng ký quyền nhà cung cấp cho thuê xe trên hệ thống CRP của bạn đã thành công. Cảm ơn bạn đã chọn đồng hành cùng chúng tôi."
@@ -65,8 +65,8 @@ namespace CRP.Models.Entities.Services
 			{
 				Credentials = new System.Net.NetworkCredential()
 				{
-					UserName = "siunhandiendien11@gmail.com",
-					Password = "0975420837"
+					UserName = "crpservices1@gmail.com",
+					Password = "A123456z"
 				},
 				EnableSsl = true
 			};
@@ -76,7 +76,7 @@ namespace CRP.Models.Entities.Services
 		//gui mail khi bi admin BAN
 		public void SendLockoutAlertEmail(AspNetUser user)
 		{
-			var mailMessage = new MailMessage("siunhandiendien11@gmail.com", user.Email)
+			var mailMessage = new MailMessage("crpservices1@gmail.com", user.Email)
 			{
 				Subject = "Tài khoản CRP của bạn đã bị khóa.",
 				Body = "Tài khoản CRP với username " + user.UserName + " của bạn đã bị khóa. Nếu có vấn đề thắc mắc, vui lòng liên hệ trực tiếp với chúng tôi.\nXin cảm ơn."
@@ -86,8 +86,8 @@ namespace CRP.Models.Entities.Services
 			{
 				Credentials = new System.Net.NetworkCredential()
 				{
-					UserName = "siunhandiendien11@gmail.com",
-					Password = "0975420837"
+					UserName = "crpservices1@gmail.com",
+					Password = "A123456z"
 				},
 				EnableSsl = true
 			};
@@ -97,7 +97,7 @@ namespace CRP.Models.Entities.Services
 		// //gui mail khi bi dang ki
 		public void SendRegistrationConfirmEmail(string userEmail, string callbackUrl)
 		{
-			var mailMessage = new MailMessage("siunhandiendien11@gmail.com", userEmail)
+			var mailMessage = new MailMessage("crpservices1@gmail.com", userEmail)
 			{
 				Subject = "Xác nhận đăng ký tài khoản CRP",
 				Body = "Vui lòng xác nhận tài khoản CRP của bạn bằng cách click vào <a href=\"" + callbackUrl + "\">đây</a>."
@@ -106,8 +106,8 @@ namespace CRP.Models.Entities.Services
 			var smtpClient = new SmtpClient("smtp.gmail.com", 587);
 			smtpClient.Credentials = new System.Net.NetworkCredential()
 			{
-				UserName = "siunhandiendien11@gmail.com",
-				Password = "0975420837"
+				UserName = "crpservices1@gmail.com",
+				Password = "A123456z"
 			};
 			smtpClient.EnableSsl = true;
 			smtpClient.Send(mailMessage);
@@ -116,7 +116,7 @@ namespace CRP.Models.Entities.Services
 		//gui mail lay lai password
 		public void SendRecoverPasswordEmail(string userEmail, string callbackUrl)
 		{
-			var mailMessage = new MailMessage("siunhandiendien11@gmail.com", userEmail)
+			var mailMessage = new MailMessage("crpservices1@gmail.com", userEmail)
 			{
 				Subject = "Khôi phục mật khẩu tài khoản CRP.",
 				Body = "Chúng tôi vừa nhận được yêu cầu khôi phục mật khẩu cho tài khoản đăng ký với email này. Để khôi phục mật khẩu, vui lòng click vào <a href=\"" + callbackUrl + "\">đây</a>."
@@ -126,8 +126,8 @@ namespace CRP.Models.Entities.Services
 			{
 				Credentials = new System.Net.NetworkCredential()
 				{
-					UserName = "siunhandiendien11@gmail.com",
-					Password = "0975420837"
+					UserName = "crpservices1@gmail.com",
+					Password = "A123456z"
 				},
 				EnableSsl = true
 			};
